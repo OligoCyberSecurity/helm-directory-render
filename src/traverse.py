dc84a0b0cbbb1a2311258751b7fd34bc23c88eac
nirds <nir@nrdlabs.com>
2024-12-11 17:23:22 +0200

    travesrse

--- src/traverse.py
+++ src/traverse.py
@@ -21,12 +21,26 @@ if env and root_dir and pattern:
     for item in os.walk(root_dir):
         # print(item[0])
         item_path = Path(item[0])
+        env_item_path = item_path.absolute().as_posix()+ "/" + env
         rendered_path = item_path.parent.absolute().as_posix() + "/" + rendered_path_input
         
         # logging.info("Checking file: " + item[0]+ " " + item[1]) 
-        if pattern_reg.search(item_path.absolute().as_posix()) and os.path.exists(rendered_path):
-            logging.info(rendered_path)
-            logging.info("Found file: " + item[0] + " " + rendered_path)
+        if pattern_reg.search(env_item_path) and os.path.exists(rendered_path):
+            
+            logging.info("Found dir: " + env_item_path + " " + rendered_path)
+            jobs_list = os.listdir(env_item_path)
+            logging.info("Jobs list: " + str(jobs_list))
+            for job in jobs_list:
+                base_job_path = item_path.absolute().as_posix() + "/base/" + job
+                job_path = env_item_path + "/" + job
+                logging.info("Job path: " + job_path)
+                job_rendered_path = rendered_path + "/"+env  +"/"  + job
+                logging.info("Job rendered path: " + job_rendered_path)
+                if os.path.exists(job_rendered_path):
+                    logging.info("Job rendered path exists: " + job_rendered_path)
+                    output.append(job)
+                else:
+                    logging.info("Job rendered path does not exist: " + job_rendered_path)
             output.append(item[0])
 else:
     exit('ERROR: the INPUT_NUM or INPUT_ENVIRONMENT is not provided')
