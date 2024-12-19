2dca278b9dc7d73e7b4a1135021926c1bd52a3f8
nirds <nir@nrdlabs.com>
2024-12-19 13:03:51 +0200

    lint

--- src/models.py
+++ src/models.py
@@ -1,11 +1,10 @@
+import logging
 import os
 import shutil
 import subprocess
-from typing import Optional
 
 from pydantic import BaseModel, Field
 
-import logging
 
 class TraversalRecord(BaseModel):
     """_description_
@@ -24,10 +23,11 @@ class TraversalRecord(BaseModel):
 
     def helm_template(self):
         if not os.path.exists(self.rendered_path):
-            logging.warning("Job path does not exist: %s creating it", self.rendered_path)
+            logging.warning(
+                "Job path does not exist: %s creating it", self.rendered_path)
             os.makedirs(self.rendered_path)
         else:
-            shutil.rmtree(self.rendered_path+ "/")
+            shutil.rmtree(self.rendered_path + "/")
         res = subprocess.run(self.helm_exec_string(), shell=True, check=True)
-        
+
         res.check_returncode()
