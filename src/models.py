9d5908df6e46cbf81e0f216d8c1b9c5915d72322
Adam Kobi <adam@oligosecurity.io>
2024-12-22 15:50:55 +0200

    fix: allow using global values and minor fixes

--- src/models.py
+++ src/models.py
@@ -12,17 +12,31 @@ class TraversalRecord(BaseModel):
         BaseModel (_type_): _description_
     """
     base_path: str
-    env_path: str
+    values_path: str
+    env: str
     chart_name: str
-    rendered_path: str
+    output_base_path: str
     name: str = Field(init=True)
+    use_globals: bool = Field(default=False)
+
+    def get_values_path(self, values_file: str):
+        return f"{self.values_path}/{values_file}"
+
+    @property
+    def output_dir(self):
+        return f"{self.output_base_path}/{self.env}/{self.name}"
 
     def helm_exec_string(self):
-        return f"helm template {self.name} {self.chart_name} -f {self.base_path} -f {self.env_path} --output-dir {self.rendered_path}"
+        cmd = ["helm", "template", self.name, self.chart_name]
+        if self.use_globals:
+            cmd.extend(["-f", f"{self.base_path}/globals.yaml"])
+        cmd.extend(["-f", self.get_values_path("base.yaml"), "-f",
+                   self.get_values_path(f"{self.env}.yaml"), "--output-dir", self.output_dir])
+        return ' '.join(cmd)
 
     def helm_template(self):
-        if os.path.exists(self.rendered_path):
-            shutil.rmtree(self.rendered_path + "/")
+        if os.path.exists(self.output_dir):
+            shutil.rmtree(self.output_dir + "/")
 
         res = subprocess.run(self.helm_exec_string(), shell=True, check=True)
 
--- src/traverse.py
+++ src/traverse.py
@@ -6,6 +6,7 @@ import logging
 import json
 from pathlib import Path
 from models import TraversalRecord
+from typing import List
 # Configure logging
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
@@ -16,6 +17,8 @@ root_dir = os.environ.get("INPUT_DIRECTORY")
 pattern = os.environ.get("INPUT_PATTERN")
 chart_name = os.environ.get("INPUT_CHART")
 rendered_path_input = os.environ.get("INPUT_RENDERED_PATH", "rendered").strip()
+use_globals = os.environ.get(
+    "INPUT_USE_GLOBALS", "false").strip().lower() == "true"
 # Check if required inputs are provided
 if not env or not root_dir or not pattern or not chart_name:
     logging.error("Missing required input")
@@ -23,17 +26,24 @@ if not env or not root_dir or not pattern or not chart_name:
 PATTERN_REG = re.compile(pattern)  # Match all files ending with input pattern"
 
 output = list()
-for root, _, _ in os.walk(root_dir):
-    item_path = Path(root)
-    env_item_path = item_path.absolute().as_posix()
-    jobs_list = os.listdir(env_item_path)
-    rendered_path = f"{item_path.parent.absolute().as_posix()}/{rendered_path_input}"
-    if PATTERN_REG.search(env_item_path) and os.path.exists(rendered_path):
-        logging.info("Found dir: %s %s", env_item_path, rendered_path)
-        for job in jobs_list:
-            t_record = TraversalRecord(base_path=f"{env_item_path}/{job}/base.yaml", env_path=f"{env_item_path}/{job}/{env}.yaml", rendered_path=f"{rendered_path}/{env}/{job}", name=job, chart_name=chart_name)
-            t_record.helm_template()
-            output.append(t_record)
+output_base_path = f"{root_dir}/{rendered_path_input}"
+templates: List[TraversalRecord] = []
+for root, dir, _ in os.walk(root_dir):
+    for d in dir:
+        full_path = os.path.join(root, d)
+        if PATTERN_REG.search(full_path):
+            logging.info("Found dir: %s", full_path)
+            templates.append(TraversalRecord(base_path=root,
+                                             values_path=full_path,
+                                             output_base_path=output_base_path,
+                                             env=env,
+                                             name=d,
+                                             chart_name=chart_name,
+                                             use_globals=use_globals))
+
+for template in templates:
+    template.helm_template()
+    output.append(template)
 
 # to set output, print to shell in following syntax
 print("::set-output name=jobs:: " +
